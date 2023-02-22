552e13ad9c5aaca9297ff17cec519e41af898774
David Jak <david.jak@student.hku.nl>
2023-02-22 14:52:50 +0100

    new island and camerascript changes

--- Project Context 2/Assets/Scripts/PlayerCamera.cs
+++ Project Context 2/Assets/Scripts/PlayerCamera.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public Transform player;
+    public Vector3 cameraOffset;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 7.7f, -7.05f);
+        transform.position = player.transform.position + cameraOffset;
     }
 }
