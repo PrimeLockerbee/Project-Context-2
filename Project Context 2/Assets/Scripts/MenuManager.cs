6b9eabfb5bc4c12dc02ea71a073552f6e9f04796
Bradley van Ewijk <32265034+PrimeLockerbee@users.noreply.github.com>
2023-03-20 14:48:05 +0100

    Final scene setup

--- Project Context 2/Assets/Scripts/MenuManager.cs
+++ Project Context 2/Assets/Scripts/MenuManager.cs
@@ -5,33 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject menu;
-    public GameObject endcam;
-    public GameObject minimap;
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            if (menu.activeSelf.Equals(false))
-            {
-                menu.SetActive(true);
-                minimap.SetActive(false);
-            }
-            else
-            {
-                menu.SetActive(false);
-                minimap.SetActive(true);
-                endcam.SetActive(false);
-            }
-            
-        }
-    }
-    /*
     public void SceneLoader(int num)
     {
         SceneManager.LoadScene(num);
     }
-    */
+
     public void Quit()
     {
         Application.Quit();
