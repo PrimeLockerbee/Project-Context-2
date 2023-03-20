4250aca029f634fbde2788de794c875521840afd
Bradley van Ewijk <32265034+PrimeLockerbee@users.noreply.github.com>
2023-03-20 17:48:48 +0100

    Bug Fixes

--- Project Context 2/Assets/Scripts/MenuManager.cs
+++ Project Context 2/Assets/Scripts/MenuManager.cs
@@ -5,6 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject _miniMap;
+    [SerializeField] GameObject _endCam;
+    [SerializeField] GameObject _menu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (_menu.activeSelf.Equals(false))
+            {
+                _menu.SetActive(true);
+                _miniMap.SetActive(false);
+            }
+            else
+            {
+                _menu.SetActive(false);
+                _miniMap.SetActive(true);
+                _endCam.SetActive(false);
+            }
+        }
+    }
+
     public void SceneLoader(int num)
     {
         SceneManager.LoadScene(num);
